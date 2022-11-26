a4b71b4bef322b64bee84fe67a9a9c055d5c9834
Lars Erik Bakkland Moi <lemoi18@uia.no>
2022-11-26 12:25:52 +0100

    Add project files.

---
+++ Converter/Docmanager/IDocmanager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter.Docmanager
+{
+    public interface IDocmanager
+    {
+
+        public void CreateUnit()
+        {
+
+        }
+
+        public void EditUnit()
+        {
+
+        }
+
+        public void DeleteUnit()
+        {
+
+        }
+
+        public void ListUnits()
+        {
+
+        }
+
+        public void GetUnit()
+        {
+
+        }
+
+        public void EditGroup()
+        {
+
+        }
+
+        public void DeleteGroup()
+        {
+
+        }
+
+        public void AddUnit()
+        {
+
+        }
+
+        public void RemoveUnit()
+        {
+
+        }
+
+        public void ListGroups()
+        {
+
+        }
+
+        public void ReadGroup()
+        {
+
+        }
+
+    }
+
+}
---
+++ Converter/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("What is your name?");
+
+
+static int Main(string[] args)
+{
+    string kuk;
+    kuk = Console.ReadLine();
+    string[] arguments = Environment.GetCommandLineArgs();
+    Console.WriteLine("GetCommandLineArgs: {0}", string.Join(", ", arguments));
+    Console.WriteLine("What is your name?");
+    var name = Console.ReadLine();
+    var currentDate = DateTime.Now;
+    Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+    Console.Write($"{Environment.NewLine}Press any key to exit...");
+    Console.ReadKey(true);
+    return 0;
+}
