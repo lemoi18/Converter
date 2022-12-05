6d64043957305e0b7aef5193e127bc34bce45c4a
Ole Markus Kvisgaard Roland <omroland@student.uia.no>
2022-12-05 21:13:25 +0100

    Added functionality to add quantity types and list properties

--- Docmanager/Program.cs
+++ Docmanager/Program.cs
@@ -11,9 +11,4 @@ using System.Xml.Linq;
 IDocmanager docmanager;
 docmanager = DocFactory.CreateDocmanager("Test");
 
-List<string> test = docmanager.ReadKeys("inverse henry");
-
-foreach (string key in test)
-{
-    Console.WriteLine(key);
-}
+Console.WriteLine(docmanager.AddQuantityType("per Kelvin", "Test"));
