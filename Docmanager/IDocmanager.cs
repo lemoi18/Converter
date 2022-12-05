5a0b6f737a260eea7a08c56f86cb60666b97fd4e
Lars Erik Bakkland Moi <lemoi18@uia.no>
2022-12-05 18:46:38 +0100

    ,

--- Docmanager/IDocmanager.cs
+++ Docmanager/IDocmanager.cs
@@ -1,11 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
-
 namespace Docmanager
 {
     public interface IDocmanager
